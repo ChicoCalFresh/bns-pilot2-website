d56521aefc32b261533133491c44a1d46eae8847
Shady-Shamy <shadyshamy80@gmail.com>
2024-05-07 10:12:46 -0700

    Added new CHC logo

--- common_code.R
+++ common_code.R
@@ -134,6 +134,7 @@ prep_binary_vars <- function(question, xlabels) {
 }
 
 # Load data
-load("../data/BNSp2_clean.Rdata")
+PATH_TO_BOX <- "C:/Users/shady/Box/" # Shady
+load(paste0(PATH_TO_BOX, "CHC All/01. Projects Active/CFO (SP6495203)/08. Research and Evaluation/02. Data Analysis/BNS3-pilot2/data/BNSp2_clean.Rdata"))
 bns <- BNSp2b
 rm(BNSp2b)
